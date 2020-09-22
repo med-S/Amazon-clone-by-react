46829e82f18df2c257e97f71ac8139ec69593903
med-S <med.salhaoui97@gmail.com>
2020-09-22 20:18:03 +0100

    build

---
+++ precache-manifest.46be0a38098af62703487d347a5c0d5e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1ec12fc154c9e46d16ee346d317a7a0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6ac1147b224d13921f20",
+    "url": "/static/css/main.6cad7f02.chunk.css"
+  },
+  {
+    "revision": "e15193c01034c4df27ff",
+    "url": "/static/js/2.fb507d0f.chunk.js"
+  },
+  {
+    "revision": "5f8be6edcb631ea3c834b8459dc2c264",
+    "url": "/static/js/2.fb507d0f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ac1147b224d13921f20",
+    "url": "/static/js/main.9e1a0c83.chunk.js"
+  },
+  {
+    "revision": "92a146e5ab9e0f9e785b",
+    "url": "/static/js/runtime-main.90140090.js"
+  }
+]);
